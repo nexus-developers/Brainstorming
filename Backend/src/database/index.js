aa84cee27db2e443a4a39a5452aa402c93f46058
Lamithy <anderson001749@gmail.com>
2020-10-10 18:28:15 -0300

    add Avatar

--- Backend/src/database/index.js
+++ Backend/src/database/index.js
@@ -6,15 +6,18 @@ import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import Post from '../app/models/Post';
 import Comment from '../app/models/Comment';
+import File from '../app/models/File';
 
 const connection = new Sequelize(databaseConfig);
 
 User.init(connection);
 Post.init(connection);
 Comment.init(connection);
+File.init(connection);
 
 Post.associate(connection.models);
 Comment.associate(connection.models);
+User.associate(connection.models);
 
 module.exports = connection;
 
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import PostController from './app/controllers/PostController';
 import CommentController from './app/controllers/CommentController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -20,6 +24,8 @@ routes.get('/post', authMiddleware, PostController.index);
 routes.get('/post/:id', authMiddleware, PostController.findOne);
 routes.get('/post-findAll', PostController.findAll);
 routes.put('/post/:id', authMiddleware, PostController.update);
+
+routes.post('/files', authMiddleware, upload.single('file'), FileController.store)
 
 
 export default routes;
