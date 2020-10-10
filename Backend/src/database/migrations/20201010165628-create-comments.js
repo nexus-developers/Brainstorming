b28bac358910e808b475020b2f0c00fa95d38226
Lamithy <anderson001749@gmail.com>
2020-10-10 16:02:46 -0300

    add comments

---
+++ Backend/src/database/migrations/20201010165628-create-comments.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('comments', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'posts', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable('comments');
+  }
+};
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import PostController from './app/controllers/PostController';
+import CommentController from './app/controllers/CommentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,7 +12,14 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.post('/post', authMiddleware, PostController.store);
+routes.post('/comment', authMiddleware, CommentController.store);
+routes.get('/comment', authMiddleware, CommentController.index);
 routes.put('/users', authMiddleware ,UserController.update);
 routes.get('/users', authMiddleware, UserController.index);
+routes.get('/post', authMiddleware, PostController.index);
+routes.get('/post/:id', authMiddleware, PostController.findOne);
+routes.get('/post-findAll', PostController.findAll);
+routes.put('/post/:id', authMiddleware, PostController.update);
+
 
 export default routes;
