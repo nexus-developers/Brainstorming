4b131b1482dd0a78d0ceebd7cc0f5cd451724a77
João Muribeca <53870325+CheffJ@users.noreply.github.com>
2020-10-11 17:36:27 -0300

    Problema de autenticação resolvido

--- Mobile/src/Routes/Auth.routes.js
+++ Mobile/src/Routes/Auth.routes.js
@@ -9,6 +9,10 @@ import Wellcome from '../Screens/Wellcome/Wellcome'
 import Login from '../Screens/Login/Login'
 import PostDetails from '../Screens/PostDetails/PostDetails'
 import CreatePost from '../Screens/CreatePost/CreatePost'
+import Register from '../Screens/Register/Register'
+import FormRegister from '../Screens/Register/FormRegister/FormRegister'
+import AreasRegister from '../Screens/Register/Areas/Areas'
+import TagsRegister from '../Screens/Register/Tags/Tags'
 
 const Stack = createStackNavigator()
 
@@ -63,6 +67,56 @@ export default function Routes(){
               component={CreatePost}
               options={{
                 headerShown: false
+              }}
+            />
+
+            <Stack.Screen 
+              name='Register' 
+              component={Register} 
+              options={{
+                headerShown: false
+              }}
+            />
+
+            <Stack.Screen 
+              name='FormRegister'
+              component={FormRegister}
+              options={{
+                title: 'Formulário de Registro',
+                headerTitleStyle: { 
+                  fontFamily: 'Bold',
+                },
+                headerTintColor: '#3B2C33',
+                headerTitleAlign: 'center',
+                headerBackTitleVisible: false,
+              }}
+            />
+
+            <Stack.Screen 
+              name='AreasRegister'
+              component={AreasRegister}
+              options={{
+                title: 'Área de Atuação',
+                headerTitleStyle: { 
+                  fontFamily: 'Bold',
+                },
+                headerTintColor: '#3B2C33',
+                headerTitleAlign: 'center',
+                headerBackTitleVisible: false,
+              }}
+            />
+
+            <Stack.Screen 
+              name='TagsRegister'
+              component={TagsRegister}
+              options={{
+                title: 'Seleção de Tags',
+                headerTitleStyle: { 
+                  fontFamily: 'Bold',
+                },
+                headerTintColor: '#3B2C33',
+                headerTitleAlign: 'center',
+                headerBackTitleVisible: false,
               }}
             />
 
