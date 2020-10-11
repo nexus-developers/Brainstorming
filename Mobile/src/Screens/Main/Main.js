a56948f10848af2b14993c82aff34b5566bc004b
Geraldo-Thorpe <64435314+geraldo-thorpe-innovation@users.noreply.github.com>
2020-10-11 10:26:14 -0300

    main

--- Mobile/src/Screens/Main/Main.js
+++ Mobile/src/Screens/Main/Main.js
@@ -1,17 +1,134 @@
 import React from 'react';
-import { View, ScrollView } from 'react-native';
-import { Fontisto } from '@expo/vector-icons';
+import { ScrollView, View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
+import { Fontisto, Feather, Ionicons, EvilIcons } from '@expo/vector-icons';
 
-import { Container, HeaderCont, ScreenTitle } from './styles';
+import post1 from './Assets/1.jpg'
+
+import { 
+  Container, 
+  HeaderCont, 
+  ScreenTitle,
+  User,
+  UserPhoto,
+  CreatePostComponent,
+  Comment,
+  Title
+} from './styles';
+
+const post = [
+  {
+    id: 1,
+    title: 'Aplicativo de Mensagens',
+    comments: 20,
+    tag: 'Tecnologia'
+  },
+  {
+    id: 2,
+    title: 'Mercado Livre de Livros',
+    comments: 100,
+    tag: 'Leitura'
+  },
+  {
+    id: 3,
+    title: 'Aplicativo de Mensagens',
+    comments: 20,
+    tag: 'Tecnologia'
+  },
+  {
+    id: 4,
+    title: 'Mercado Livre de Livros',
+    comments: 100,
+    tag: 'Leitura'
+  },
+  {
+    id: 5,
+    title: 'Aplicativo de Mensagens',
+    comments: 20,
+    tag: 'Tecnologia'
+  },
+  {
+    id: 6,
+    title: 'Mercado Livre de Livros',
+    comments: 100,
+    tag: 'Leitura'
+  },
+  {
+    id: 7,
+    title: 'Aplicativo de Mensagens',
+    comments: 20,
+    tag: 'Tecnologia'
+  },
+  {
+    id: 8,
+    title: 'Mercado Livre de Livros',
+    comments: 100,
+    tag: 'Leitura'
+  },
+  
+
+]
 
 const Main = () => {
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: 'blue' }}>
+    <ScrollView style={{ flex: 1, backgroundColor: "#FFF" }}>
       <Container>
         <HeaderCont>
-          <Fontisto style={{ marginLeft: 15 }} name="atom" size={24} color="black" />
-          <ScreenTitle>Brainstorming</ScreenTitle>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Fontisto name="atom" size={24} color="black" />
+            <ScreenTitle>Brainstorming</ScreenTitle>
+          </View>
+          <User>
+            <UserPhoto
+              source={{ uri: 'https://api.adorable.io/avatars/50/abott@adorable.png' }}
+            />
+          </User>
         </HeaderCont>
+        <CreatePostComponent>
+          <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 10 }}>
+              <View style={{ backgroundColor: '#66A3FE', padding: 6, borderRadius:10, alignItems: 'center' }}>
+                <Feather name="send" size={20} color="#FFF" />
+              </View>
+            </View>
+            <View style={{ flexDirection: 'column' }}>
+              <Comment>Teve alguma ideia bacana?</Comment>
+              <Title>Criar uma publicação</Title>
+            </View>
+          </View>
+          <Ionicons name="ios-arrow-forward" size={20} color="#CACACA" style={{ marginRight: 15 }} />
+        </CreatePostComponent>
+
+        <View style={{ marginTop: 30, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 15 }}>
+          <Text style={{ fontFamily: 'Regular', color: '#CACACA' }}>NOSSA COMUNIDADE</Text>
+          <TouchableOpacity>
+            <Text style={{ fontFamily: 'Bold', color: '#66A3FE' }}>Ver Todos</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList 
+          data={post}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <Image 
+                source={post1}
+              />
+              <View>
+                <View>
+                  <Title>{item.title}</Title>
+                  <Comment>{item.title}</Comment>
+                </View>
+                <View>
+                  <TouchableOpacity>
+                    <Ionicons name="ios-arrow-forward" size={20} color="#000" />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <EvilIcons name="comment" size={24} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
       </Container>
     </ScrollView>
   )
--- Mobile/src/Screens/Main/styles.js
+++ Mobile/src/Screens/Main/styles.js
@@ -7,9 +7,8 @@ export const Container = styled.View`
 
 export const HeaderCont = styled.View`
   flex-direction: row;
-  width: 100%;
-  height: 100%;
-  background-color: red;
+  margin: 20px 15px 0;
+  justify-content: space-between;
 `;
 
 export const ScreenTitle = styled.Text`
@@ -18,3 +17,32 @@ export const ScreenTitle = styled.Text`
   font-family: 'Bold';
   margin-left: 5px;
 `;
+
+export const User = styled.TouchableOpacity``
+export const UserPhoto = styled.Image`
+  height: 40px;
+  width: 40px;
+  border-width: 1px;
+  border-radius: 50px;
+`
+
+export const CreatePostComponent = styled.TouchableOpacity`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #F7F8FA;
+  margin: 20px 20px 0;
+  padding: 10px;
+  border-radius: 10px;
+`
+
+export const Comment = styled.Text`
+  font-family: 'Light';
+  font-size: 13px;
+  color: #3B2C33;
+`
+export const Title = styled.Text`
+  font-family: 'Bold';
+  font-size: 15px;
+  color: #3B2C33;
+`
